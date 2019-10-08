b65c08f49575e8074b47aa579579b8ff5ad446de
kcazuki <53326300+kcazuki@users.noreply.github.com>
2019-10-08 21:06:41 +0900

    tweets_controller_setting

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,17 @@
 class TweetsController < ApplicationController
   def index
+    @tweets = Tweet.order("created_at DESC")
+  end
+
+  def new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  private
+  def tweet_params
+    params.permit(:name, :image, :text)
   end
 end
